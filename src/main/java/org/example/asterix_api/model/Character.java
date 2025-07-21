bb4fd33be43440c11cae45a0bc8ee54ef6fe9f36
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2025-07-21 11:19:06 +0200

    Initial commit of AsterixAPI

---
+++ src/main/java/org/example/asterix_api/model/Character.java
@@ -0,0 +1,54 @@
+package org.example.asterix_api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("characters")
+public class Character {
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private String profession;
+
+    public Character() {}
+
+    public Character(String id, String name, int age, String profession) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+}
---
+++ src/main/java/org/example/asterix_api/repository/CharacterRepo.java
@@ -0,0 +1,8 @@
+package org.example.asterix_api.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepo extends MongoRepository<Character, String> {
+}
